aaed976a5f49a7b09d075e36c4dd8a6acf0e67c7
daddyincase168 <somonorhong011@gmail.com>
2025-05-26 21:15:53 +0700

    nothing

--- database/migrations/2025_05_26_130527_create_products_table.php
+++ database/migrations/2025_05_26_130527_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id(); // create id with primary key with auto increment
+            $table->string("product_name" , 50); // create name with varchar
+            $table->integer("quantity");
+            $table->decimal("price",10,2);
+            $table->string("image")->nullable();
+            $table->text("desciption")->nullable();
             $table->timestamps(); // timestamps create 2 field. Field1 is create_at and field2 is update_at
         });
     }
